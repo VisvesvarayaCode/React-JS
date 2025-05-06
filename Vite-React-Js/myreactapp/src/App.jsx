369b5f0977cac53a25139219b27088375ea8f23f
Nitit Tambe <tambenitin2002@gmail.com>
2025-05-06 11:55:06 +0530

    program update may 6

---
+++ Vite-React-Js/myreactapp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css"
+
+function App() {
+  return(
+    <>
+    <h1>This is heading</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem iure unde facilis explicabo corporis rerum quas accusamus quibusdam consequuntur fugit, quae repellat reiciendis magni fuga maiores corrupti at consequatur repellendus eaque consectetur! Fugit, accusamus! Laborum.
+    </p>
+    </>
+  )
+}
+export default App;
--- Vite-React-Js/myreactapp/src/main.jsx
+++ Vite-React-Js/myreactapp/src/main.jsx
@@ -1,10 +1,25 @@
-import ReactDOM, { createRoot } from 'react-dom/client'
+// import ReactDOM, { createRoot } from 'react-dom/client'
+// import React from 'react';
 
-import React from 'react';
-const { createElement } = React;
-let f = createElement("h1", { style: { color: "green", backgroundColor: "pink" } }, "Registration Form", createElement("br"), createElement("div", { id: "box" },
-  createElement("form", { action: "" }, createElement("label", { htmlFor: "name" }, "Name", createElement("input", { type: "name", id: "name", placeholder: "Enter Your Name: " })),
-    createElement("br"), createElement("label", { htmlFor: "html" }, "Email: ", createElement("input", { type: "email", id: "email", placeholder: "Enter your email: " })), createElement("br"), createElement("label", { htmlFor: "password" }, "password: ", createElement("input", { type: "password", id: "password", placeholder: "Enter your password: " }))), createElement("br"), createElement("button", null, "submit")));
+// const { createElement } = React;
+// // let demo=React.createElement(TagName,{Attribute},value)
+// let f = createElement("h1", { style: { color: "green", backgroundColor: "pink" } }, "Registration Form", createElement("br"), createElement("div", { id: "box" },
+//   createElement("form", { action: "" }, createElement("label", { htmlFor: "name" }, "Name:", createElement("input", { type: "name", id: "name", placeholder: "Enter Your Name: " })),
+//     createElement("br"), createElement("label", { htmlFor: "html" }, "Email: ", createElement("input", { type: "email", id: "email", placeholder: "Enter your email: " })), createElement("br"), createElement("label", { htmlFor: "password" }, "password: ", createElement("input", { type: "password", id: "password", placeholder: "Enter your password: " }))), createElement("br"), createElement("button", null, "submit")));
 
-createRoot(document.getElementById('root')).render();
-ReactDOM.createRoot(document.getElementById("root")).render(f);
+// // createRoot(document.getElementById('root')).render();
+// ReactDOM.createRoot(document.getElementById("root")).render(f);
+
+
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import App from "./App";
+
+function demo(){
+  return (
+   <App/> 
+  )
+  
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(demo());
